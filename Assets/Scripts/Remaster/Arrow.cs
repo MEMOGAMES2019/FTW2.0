6054750ffe5fd7694e4ea6064cb2095d30fbf34d
DavidCogolludo <davipe05@ucm.es>
2019-02-15 10:50:40 +0100

    Sonido al pulsar flecha

--- Assets/Scripts/Remaster/Arrow.cs
+++ Assets/Scripts/Remaster/Arrow.cs
@@ -7,10 +7,13 @@ public class Arrow : MonoBehaviour {
 
     public int dir;
     public Car car;
+    public AudioClip flecha;
+    AudioSource audioSource;
     GM gm;
     private void Start()
     {
         gm = GameObject.Find("GM").GetComponent<GM>();
+        audioSource = transform.parent.GetComponent<AudioSource>();
     }
     /// <summary>
     /// Comprueba si se ha hecho click sobre ella y avisa al coche.
@@ -19,6 +22,8 @@ public class Arrow : MonoBehaviour {
     {
         if (!gm.Paused)
         {
+            audioSource.clip = flecha;
+            audioSource.Play();
             switch (dir)
             {
                 case 0: car.MoveToRight(); break;
